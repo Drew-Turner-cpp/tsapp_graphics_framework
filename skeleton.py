b1418ada30a3312248df4f592661cc0081ad30ac
Drew Turner <83795882+Drew-Turner-cpp@users.noreply.github.com>
2021-12-03 15:04:49 -0600

    skeleton structure file added

---
+++ skeleton.py
@@ -0,0 +1,10 @@
+import tsapp as ts
+import framework as f
+
+
+window = ts.GraphicsWindow()  # Create GraphicsWindow
+
+
+# Main Loop
+while window.is_running:  # Keep window open until user closes it
+    window.finish_frame()
